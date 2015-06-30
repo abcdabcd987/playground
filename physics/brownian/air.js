5c6a5ce7641a1ddb9ce6a9052ce2405d26a18f4f
Lequn Chen <chenlequn22@gmail.com>
2015-06-30 23:35:15 +0800

    bugfix

--- physics/brownian/air.js
+++ physics/brownian/air.js
@@ -427,7 +427,7 @@ function update_chart() {
         axis: {
             y: {
                 label: "<R^2>",
-                padding: {top: 0, bottom: 0, left:0, right:0}
+                padding: {top: 0, bottom: 0, left:0, right:0},
                 tick: {count:4}
             },
             x: {
