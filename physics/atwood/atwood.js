540dd62011f325e866802b24ea06ed2ea78f4a23
lagoon0o0 <lagoonzxy@gmail.com>
2016-01-11 10:51:40 +0800

    ha

--- physics/atwood/atwood.js
+++ physics/atwood/atwood.js
@@ -1,13 +1,23 @@
+function debug_start() {
+    $("debug").innerHTML = "<ul>";
+}
+function debug(message) {
+    $("debug").innerHTML += "<li>" + message + "</li>";
+}
+function debug_end() {
+    $("debug").innerHTML += "</ul>";
+}
+
 function AtWood(mass_large, mass_small, theta0) {
     paper.project.clear();
 
     //------ canvas constant
     var PIVOT2 = new Point(window.innerWidth/2, window.innerHeight/2);
-    var PIVOT_DISTANCE = 200;
+    var PIVOT_DISTANCE = 100;
     var PIVOT1 = PIVOT2.add([-PIVOT_DISTANCE, 0])
     var PIVOT_RADIUS = 3;
     var INIT_RADIUS = 100;
-    var SMALL_BALL_RADIUS = 5;
+    var SMALL_BALL_RADIUS = 1;
     var LARGE_BALL_RADIUS = 10;
 
     //------ canvas objects
@@ -16,8 +26,8 @@ function AtWood(mass_large, mass_small, theta0) {
     var c_large = new Path.Circle(PIVOT1.add([0, INIT_RADIUS]), LARGE_BALL_RADIUS);
     var c_small = new Path.Circle(PIVOT2.add([0, INIT_RADIUS]), SMALL_BALL_RADIUS);
     var c_rope = new Path();
-    //var c_track = new Raster();
-    var c_track = new Path();
+    var c_track = new Raster();
+    //var c_track = new Path();
 
     c_pivot1.fillColor = 'black';
     c_pivot2.fillColor = 'black';
@@ -30,7 +40,9 @@ function AtWood(mass_large, mass_small, theta0) {
     c_rope.add(c_pivot2.position);
     c_rope.add(c_small.position);
 
-    c_track.strokeColor = 'red';
+    //c_track.strokeColor = 'red';
+    c_track.size = view.size;
+    c_track.position = view.center;
 
     //------ variables
     var M = mass_large;
@@ -40,6 +52,7 @@ function AtWood(mass_large, mass_small, theta0) {
     var pr = 0;
     var theta = theta0;
     var ptheta = 0;
+    var E =  M * g * r - m * g * r * Math.cos(theta);
     var f = [function(t, y) { return y[1] / (M+m); },
              function(t, y) { return y[3]*y[3]/(m*Math.pow(r, 3)) - M*g + m*g*Math.cos(y[2]); },
              function(t, y) { return y[3] / (m * y[0] * y[0]); },
@@ -47,10 +60,56 @@ function AtWood(mass_large, mass_small, theta0) {
 
     //------ animation
     view.onFrame = function(event) {
+        debug_start();
         var st = (new Date()).getTime();
 
+        var vr = pr / (M + m);
+
+        var vtheta = ptheta / m / r;
+        
+        var vv = Math.sqrt(vr * vr + vtheta * vtheta);
+        
+        var U = M * g * r - m * g * r * Math.cos(theta);
+        var K = (vr * vr + vtheta * vtheta) * m / 2 + (vr * vr) * M / 2;
+        {
+            var d = Math.sqrt((E - U - (M + m) * vr * vr / 2) * 2 / m);
+            var sgn = 1;
+            if(vtheta < 0)
+                sgn = -1;
+            if(r < 100) {
+                ptheta = sgn * d * m * r;
+            }
+        }
+        {
+            var d = Math.sqrt((E - U - m * vtheta * vtheta / 2) * 2 / (m+M));
+            var sgn = 1;
+            if(pr < 0)
+                sgn = -1;
+            if(r > 100) {
+                pr = sgn * d * (m+M);
+            }
+        }
+        debug(d);
+        debug(vr);
+        debug(r);
+        debug(r);
+        debug(K);
+        debug(U);
+        debug(K+U);
+        for (var T = 0; T < 50; ++T) {
+
         var y0 = [r, pr, theta, ptheta];
-        var y = runge_kutta(f, 0, y0, demo_speed);
+        var y;
+        if(r < 30) {
+            if(r < 10)
+                y = runge_kutta(f, 0, y0, demo_speed/20);
+            else {
+                y = runge_kutta(f, 0, y0, demo_speed/20);
+            }
+        } else {
+
+            y = runge_kutta(f, 0, y0, demo_speed/10);
+        }
         r = y[0], pr = y[1], theta = y[2], ptheta = y[3];
 
         c_large.position.y = PIVOT1.y + 2*INIT_RADIUS - r;
@@ -60,10 +119,13 @@ function AtWood(mass_large, mass_small, theta0) {
         c_small.position.y = PIVOT2.y + r * Math.cos(theta);
         c_rope.segments[3].point = c_small.position;
 
-        c_track.add(c_small.position);
+        //c_track.add(c_small.position);
+         c_track.setPixel(c_small.position, 'red');
+        }
 
         var ed = (new Date()).getTime();
         document.getElementById("elapse-time").innerHTML = (ed-st).toFixed(0) + "ms"; 
+        debug_end();
     }
 }
 
@@ -71,4 +133,4 @@ function set_demo_speed(event) {
     demo_speed = document.getElementById('speed-slider').value;
 }
 
-AtWood(2.394, 1, Math.PI/2);
+AtWood(21, 1, 90 / 180 * Math.PI);
